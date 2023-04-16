0f491edb9b10e707810b4b1575b3111565b838ab
Andrew Q <aquesnel@earthware.me>
2023-04-15 19:08:37 -0600

    - Added initial script - Updated readme

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # pegasus-steam-wrapper
-A quick and dirty Steam wrapper for Pegasus Frontend to ensure Steam closes once the game is closed
+A quick and dirty Steam wrapper for Pegasus Frontend to ensure Steam closes once the game is closed.
+
+## Notes
+This currently only works with the FlatPak version of Pegasus and the local version of Steam.
+
+## Installation
+Run the following commands:
+
+```
+wget https://raw.githubusercontent.com/amura11/pegasus-steam-wrapper/main/steam-wrapper.sh
+sudo mv steam-wrapper.sh /usr/local/bin/steam
+sudo chmod +x /usr/local/bin/steam
+```
+
+Run Steam games from Pegasus!
---
+++ steam-wrapper.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+parent_process=$(ps -o comm= $(ps -o ppid= $PPID))
+
+# Check if the caller is systemd, this is likely Pegasus calling it via flatpak-spawn
+if [[ $parent_process == "systemd" ]]; then
+  # Get the first argument
+  first_arg="$1"
+
+  # Check if the first argument has the format "steam://rungameid/1234567"
+  if [[ "$first_arg" =~ ^steam://rungameid/([0-9]+)$ ]]; then
+    
+    # Extract the numbers from the first argument
+    game_id="${BASH_REMATCH[1]}"
+
+    # Launch the game with the specified ID
+    /usr/games/steam -silent "$@" 2>&1 | tee >(
+      while read line; do
+        if [[ $line == *"Game process removed: AppID game_id"* ]]; then
+          sleep 10; steam -shutdown; break
+        fi
+      done
+      )
+  fi
+else
+  # If the calling process is not systemd, or the first argument does not match the expected format,
+  # execute the original steam command with the provided arguments
+  /usr/games/steam "$@"
+fi
